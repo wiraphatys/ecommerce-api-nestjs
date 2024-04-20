42f7be08668f9b4085e95006d86cd2f2f307790a
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-04-20 20:39:05 +0700

    add: init products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
