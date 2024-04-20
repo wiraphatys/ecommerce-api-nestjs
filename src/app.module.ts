dc44ffb3532db5c7affea5d7cb40bd199bfad6ec
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-04-20 20:40:02 +0700

    add: init orders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, ProductsModule],
+  imports: [DatabaseModule, UsersModule, ProductsModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
