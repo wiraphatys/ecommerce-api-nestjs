8cb62ca460ad8b924ac167dbb7964be0f2ed156c
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-04-20 21:29:27 +0700

    update: set global prefix and use middleware cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // middleware
+  app.use(cookieParser());
+
+  // listen on port 3000 and set global prefix
+  app.setGlobalPrefix("api/v1");
   await app.listen(3000);
 }
 bootstrap();
