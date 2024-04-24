9e51a8d62cb138bcf81212a653d0c79de8f805be
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-04-25 03:37:44 +0700

    add: database module into orders

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
